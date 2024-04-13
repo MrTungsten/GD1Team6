85fa00fbeeed8e5d90eaeb320ea070c61c3f6667
Max Tung <maxhullms@gmail.com>
2024-04-13 05:33:55 -0400

    LeaderboardInputChange

--- StarFighterLegends/Assets/Scripts/HighScoreScripts/HighScoreInput.cs
+++ StarFighterLegends/Assets/Scripts/HighScoreScripts/HighScoreInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,28 +7,39 @@ using UnityEngine.UI;
 
 public class HighScoreInput : MonoBehaviour
 {
+
+    public enum CurrentInitial
+    {
+        First,
+        Second,
+        Third
+    }
+
     private HighScoreTableScript highScoreTableScript;
     private GameObject table;
-    private Transform playerInputBox;
     private Transform playerNameInput;
-    private char playerFirstInitial;
-    private char playerSecondInitial;
-    private char playerThirdInitial;
-    private char[] listOfAlphabet = new char[]
-    {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 
-    };
+    private CurrentInitial currentInitial = CurrentInitial.First;
+    private GameObject playerFirstInitial;
+    private GameObject playerSecondInitial;
+    private GameObject playerThirdInitial;
+    private TextMeshProUGUI playerFirstInitialText;
+    private TextMeshProUGUI playerSecondInitialText;
+    private TextMeshProUGUI playerThirdInitialText;
+    private TextMeshProUGUI currentCharText;
+    private TextMeshProUGUI previousCharText;
+    private TextMeshProUGUI nextCharText;
+    private char[] listOfAlphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     private char currentlySelectedChar = 'A';
     private string playerName = "";
-    private bool submitted = false;
+    private float intiialTimer = 0f;
+    private float initalScrollCooldown = 0.5f;
+    private float scrollTimer = 0f;
+    private float scrollCooldown = 0.15f;
 
     private void Awake()
     {
 
         playerNameInput = transform.Find("NameInputScreen");
-
-        playerInputBox = playerNameInput.Find("PlayerNameInput");
-        playerInputBox.GetComponent<TextMeshProUGUI>().text = playerName;
 
         highScoreTableScript = GameObject.FindAnyObjectByType<HighScoreTableScript>();
         table = GameObject.Find("Table");
@@ -35,13 +47,13 @@ public class HighScoreInput : MonoBehaviour
         if (highScoreTableScript.ReturnValidHighScore())
         {
             table.SetActive(false);
-
-            foreach (Button button in listOfAlphas)
-            {
-                button.onClick.AddListener(() => { AddAlpha(button); });
-            }
-
-            listOfAlphas[0].Select();
+            playerFirstInitial = GameObject.Find("FirstNameInitial");
+            playerSecondInitial = GameObject.Find("SecondNameInitial");
+            playerThirdInitial = GameObject.Find("ThirdNameInitial");
+            playerFirstInitialText = playerFirstInitial.transform.Find("FirstInitialText").GetComponent<TextMeshProUGUI>();
+            playerSecondInitialText = playerSecondInitial.transform.Find("SecondInitialText").GetComponent<TextMeshProUGUI>();
+            playerThirdInitialText = playerThirdInitial.transform.Find("ThirdInitialText").GetComponent<TextMeshProUGUI>();
+            currentCharText = playerFirstInitialText;
         }
         else
         {
@@ -54,29 +66,166 @@ public class HighScoreInput : MonoBehaviour
 
     private void Update()
     {
-        ActivateHighScoreTable();
+        CurrentAlpha();
+    }
+
+    private void CurrentAlpha()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentlySelectedChar = GetAlpha(currentlySelectedChar, "previous");
+            UpdateText(currentCharText, false);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (intiialTimer > initalScrollCooldown)
+            {
+                if (scrollTimer > scrollCooldown)
+                {
+                    currentlySelectedChar = GetAlpha(currentlySelectedChar, "previous");
+                    UpdateText(currentCharText, false);
+                    scrollTimer = 0f;
+                }
+                else
+                {
+                    scrollTimer += Time.deltaTime;
+                }
+            }
+            else
+            {
+                intiialTimer += Time.deltaTime;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            currentlySelectedChar = GetAlpha(currentlySelectedChar, "next");
+            UpdateText(currentCharText, false);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (intiialTimer > initalScrollCooldown)
+            {
+                if (scrollTimer > scrollCooldown)
+                {
+                    currentlySelectedChar = GetAlpha(currentlySelectedChar, "next");
+                    UpdateText(currentCharText, false);
+                    scrollTimer = 0f;
+                }
+                else
+                {
+                    scrollTimer += Time.deltaTime;
+                }
+            }
+            else
+            {
+                intiialTimer += Time.deltaTime;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            playerName += currentlySelectedChar;
+            Debug.Log("Player name: " + playerName);
+            UpdateText(currentCharText, true);
+        }
+        if (Input.anyKeyDown)
+        {
+            intiialTimer = 0f;
+            scrollTimer = 0f;
+        }
+    }
+
+    private char GetAlpha(char letter, string mode)
+    {
+        int indexOfLetter = Array.IndexOf(listOfAlphabet, letter);
+
+        if (mode == "previous")
+        {
+            if (indexOfLetter == 0)
+            {
+                return 'Z';
+            }
+            else
+            {
+                return listOfAlphabet[indexOfLetter - 1];
+            }
+        }
+        else if (mode == "next")
+        {
+            if (indexOfLetter == listOfAlphabet.Length - 1)
+            {
+                return 'A';
+            }
+            else
+            {
+                return listOfAlphabet[indexOfLetter + 1];
+            }
+        }
+        else
+            return '0';
+    }
+
+    private void UpdateText(TextMeshProUGUI charToUpdate, bool submission)
+    {
+        if (submission == true)
+        {
+
+            currentlySelectedChar = 'A';
+
+            switch (currentInitial)
+            {
+                case CurrentInitial.First:
+                    playerFirstInitialText.color = new Color(0, 0, 0, 1);
+                    currentInitial = CurrentInitial.Second;
+                    playerSecondInitialText.color = new Color(0, 0, 1, 1);
+                    break;
+                case CurrentInitial.Second:
+                    playerSecondInitialText.color = new Color(0, 0, 0, 1);
+                    currentInitial = CurrentInitial.Third;
+                    playerThirdInitialText.color = new Color(0, 0, 1, 1);
+                    break;
+                case CurrentInitial.Third:
+                    ActivateHighScoreTable();
+                    break;
+            }
+        }
+        else
+        {
+            switch (currentInitial)
+            {
+                case CurrentInitial.First:
+                    charToUpdate = playerFirstInitialText;
+                    previousCharText = playerFirstInitial.transform.Find("PreviousLetter").GetComponent<TextMeshProUGUI>();
+                    nextCharText = playerFirstInitial.transform.Find("NextLetter").GetComponent<TextMeshProUGUI>();
+                    charToUpdate.text = Convert.ToString(currentlySelectedChar);
+                    previousCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "previous"));
+                    nextCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "next"));
+                    break;
+                case CurrentInitial.Second:
+                    charToUpdate = playerSecondInitialText;
+                    previousCharText = playerSecondInitial.transform.Find("PreviousLetter").GetComponent<TextMeshProUGUI>();
+                    nextCharText = playerSecondInitial.transform.Find("NextLetter").GetComponent<TextMeshProUGUI>();
+                    charToUpdate.text = Convert.ToString(currentlySelectedChar);
+                    previousCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "previous"));
+                    nextCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "next"));
+                    break;
+                case CurrentInitial.Third:
+                    charToUpdate = playerThirdInitialText;
+                    previousCharText = playerThirdInitial.transform.Find("PreviousLetter").GetComponent<TextMeshProUGUI>();
+                    nextCharText = playerThirdInitial.transform.Find("NextLetter").GetComponent<TextMeshProUGUI>();
+                    charToUpdate.text = Convert.ToString(currentlySelectedChar);
+                    previousCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "previous"));
+                    nextCharText.text = Convert.ToString(GetAlpha(Convert.ToChar(charToUpdate.text), "next"));
+                    break;
+            }
+        }
     }
 
     private void ActivateHighScoreTable()
     {
-        if (submitted)
-        {
-            table.SetActive(true);
-            highScoreTableScript.AddHighScoreEntry(ScoreManager.Instance.GetTotalScore(), playerName);
-            highScoreTableScript.SetIsViewing();
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void AddAlpha(Button button)
-    {
-        if (!(playerName.Length >= 3))
-        {
-            playerInputBox.GetComponent<TextMeshProUGUI>().text = playerName;
-        }
-        else
-        {
-            submitted = true;
-        }
+        table.SetActive(true);
+        highScoreTableScript.AddHighScoreEntry(ScoreManager.Instance.GetTotalScore(), playerName);
+        highScoreTableScript.SetIsViewing();
+        gameObject.SetActive(false);
     }
 }
